db69d9c11601eea3d05f7a3eebde7a09a46ce1f3
WonGi-Kim <kwg970310@gmail.com>
2024-03-15 21:27:01 +0900

    역시 Hello, world! 출력은 필수지

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      title: "flutter ex"
+      home: Center(child: Text("Hello, world!")),
     );
   }
 }
